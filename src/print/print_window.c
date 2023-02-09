e7c140c7fc32098dfa144f6681ae289e36923a30
Shai Halimi <shalimi@c2r9s7.42lausanne.ch>
2023-02-09 18:54:35 +0100

    merge with master

--- src/print/print_window.c
+++ src/print/print_window.c
@@ -6,7 +6,7 @@
 /*   By: ncotte <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 14:39:19 by ncotte            #+#    #+#             */
-/*   Updated: 2023/02/01 14:39:21 by ncotte           ###   ########.fr       */
+/*   Updated: 2023/02/09 18:52:04 by shalimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,12 @@ static unsigned int	find_color_pixel(t_scene *scene, t_point ray)
 {
 	unsigned int	color;
 	double			first_intersect;
+	t_objects       *obj;
 	double			tmp;
 	int				i;
 
 	i = -1;
+	obj = 0;
 	color = 0;
 	first_intersect = INFINITY;
 	while ((scene->objects)[++i])
@@ -38,10 +40,38 @@ static unsigned int	find_color_pixel(t_scene *scene, t_point ray)
 		if (!isinf(tmp) && tmp < first_intersect)
 		{
 			first_intersect = tmp;
+			obj = (scene->objects)[i];
 			color = ((scene->objects)[i])->get_color(scene, \
 				((scene->objects)[i])->object);
 		}
 	}
+    if (obj && obj->type == SPHERE)
+    {
+        t_sphere *sphere = ((t_sphere *)obj->object);
+        t_point center = sphere->coord;
+        t_point origin = scene->camera->coord;
+        t_point hit_point = add_vectors(origin, scalar_multi(first_intersect, ray));
+        t_point normal = unit_vector(sub_vectors(hit_point, center));
+        double dot = dot_product(ray, normal);
+        t_point rebound = unit_vector(sub_vectors(ray, scalar_multi(2.0 * dot, normal)));
+        t_point hit_point_to_light = unit_vector(sub_vectors(scene->light->coord, hit_point));
+        double dot2 = dot_product(hit_point_to_light, rebound);
+        double angle2 = acos(dot2);
+        t_color base = sphere->color;
+
+        base.r *= (-M_1_PI) * angle2 + 1;
+        base.g *= (-M_1_PI) * angle2 + 1;
+        base.b *= (-M_1_PI) * angle2 + 1;
+
+        if (base.r > 255) base.r = 255;
+        if (base.r < 0) base.r = 0;
+        if (base.g > 255) base.g = 255;
+        if (base.g < 0) base.g = 0;
+        if (base.b > 255) base.b = 255;
+        if (base.b < 0) base.b = 0;
+        return color_trgb(base);
+    }
+
 	return (color);
 }
 
