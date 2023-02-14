1f6b1471f06766b75cce1a3b36b36c37d14f6857
Shai Halimi <shalimi@c1r11s4.42lausanne.ch>
2023-02-14 22:42:27 +0100

    fixed ambient light

--- src/print/phong.c
+++ src/print/phong.c
@@ -6,7 +6,7 @@
 /*   By: shalimi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 22:18:57 by shalimi           #+#    #+#             */
-/*   Updated: 2023/02/13 22:35:28 by shalimi          ###   ########.fr       */
+/*   Updated: 2023/02/14 22:41:47 by shalimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 unsigned int	phong_ambient(t_amb_light *amb_light, t_color base)
 {
 	return (create_trgb(0, \
-		(amb_light->color.r + base.r) / 255 * amb_light->ratio, \
-		(amb_light->color.g + base.g) / 255 * amb_light->ratio, \
-		(amb_light->color.b + base.b) / 255 * amb_light->ratio));
+		((amb_light->color.r * base.r) / 255) * amb_light->ratio, \
+		((amb_light->color.g * base.g) / 255) * amb_light->ratio, \
+		((amb_light->color.b * base.b) / 255) * amb_light->ratio));
 }
 
 unsigned int	phong_color(t_scene *scene, t_color base, double dot)
@@ -26,9 +26,9 @@ unsigned int	phong_color(t_scene *scene, t_color base, double dot)
 	double	g;
 	double	b;
 
-	r = (scene->amb_light->color.r + base.r) / 255 * scene->amb_light->ratio;
-	g = (scene->amb_light->color.g + base.g) / 255 * scene->amb_light->ratio;
-	b = (scene->amb_light->color.b + base.b) / 255 * scene->amb_light->ratio;
+	r = ((scene->amb_light->color.r * base.r) / 255) * scene->amb_light->ratio;
+	g = ((scene->amb_light->color.g * base.g) / 255) * scene->amb_light->ratio;
+	b = ((scene->amb_light->color.b * base.b) / 255) * scene->amb_light->ratio;
 	r += (scene->light->color.r * base.r * dot / 255) * scene->light->ratio;
 	g += (scene->light->color.g * base.g * dot / 255) * scene->light->ratio;
 	b += (scene->light->color.b * base.b * dot / 255) * scene->light->ratio;
