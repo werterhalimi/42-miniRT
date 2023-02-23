a4089b43b7db2b29bbaf778da72d031624625e6b
Shai Halimi <shalimi@c2r9s3.42lausanne.ch>
2023-02-23 19:46:54 +0100

    implement texture and normal map for all shape

--- src/print/print_cone.c
+++ src/print/print_cone.c
@@ -6,11 +6,39 @@
 /*   By: ncotte <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 17:04:57 by ncotte            #+#    #+#             */
-/*   Updated: 2023/02/22 01:05:13 by shalimi          ###   ########.fr       */
+/*   Updated: 2023/02/23 18:57:54 by shalimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+static t_color	cone_map(t_texture *tex, double longitude, double h)
+{
+	double	tu;
+	double	tv;
+
+	tu = h;
+	tv = longitude * MC_1_2PI + 0.25;
+	return (tex->pixels[(int)(tu * tex->height)][(int)(tv * tex->width)]);
+}
+
+static t_color	get_uv_color(t_cone *cone, t_texture *tex, \
+					t_point hit_point)
+{
+	double		tu;
+	double		tv;
+	double		longitude;
+	t_point		vector;
+
+	vector = sub_vectors(hit_point, cone->coord);
+	longitude = (atan(dot_product(vector, cone->down) \
+		/ dot_product(vector, cone->right)));
+	if (dot_product(vector, cone->right) < 0)
+		longitude += M_PI;
+	tu = 0.5 * dot_product(vector, cone->direction) + 0.5;
+	tv = longitude * MC_1_2PI + 0.25;
+	return (tex->pixels[(int)(tu * tex->height)][(int)(tv * tex->width)]);
+}
 
 t_color	get_color_cone(t_scene *scene, t_object *object, \
 			t_point hit_point, t_point normal)
@@ -18,25 +46,28 @@ t_color	get_color_cone(t_scene *scene, t_object *object, \
 	t_cone	*cone;
 	t_point	vector;
 	double	h;
-	long	a;
+
+	double		longitude;
 
 	(void) scene;
 	(void) normal;
 	cone = (t_cone *)object->object;
-	if (!object->color_bis)
+	if (!object->color_bis && !object->texture)
 		return (cone->color);
 	vector = sub_vectors(hit_point, cone->coord);
-	a = (long)floor(atan(dot_product(vector, cone->right) \
-		/ dot_product(vector, cone->down)) * MC_8_PI);
+	longitude = (atan(dot_product(vector, cone->right) \
+		/ dot_product(vector, cone->down)));
 	h = dot_product(vector, cone->direction);
 	if (h < cone->height - FLT_EPSILON)
 	{
-		if (!((long)floor(h) % 2) ^ !(a % 2))
+		if(object->texture)
+			return (cone_map(object->texture, longitude, h / cone->height));
+		if (!((long)floor(h) % 2) ^ !((long)floor(longitude * MC_8_PI) % 2))
 			return (*object->color_bis);
 		return (cone->color);
 	}
-	if (!((long)floor(sqrt(distance_square(vector, \
-		scalar_multi(h, cone->direction)))) % 2) ^ !(a % 2))
+	if (object->color_bis && !((long)floor(sqrt(distance_square(vector, \
+		scalar_multi(h, cone->direction)))) % 2) ^ !((long) floor(longitude * MC_8_PI) % 2))
 		return (*object->color_bis);
 	return (cone->color);
 }
@@ -47,6 +78,8 @@ t_point	normal_cone(t_point ray, t_point hit_point, \
 	t_cone	*cone;
 	t_point	normal;
 	t_point	chp;
+	t_point		perturbation;
+	t_color		color;
 	t_point	y;
 	double	t;
 
@@ -56,6 +89,13 @@ t_point	normal_cone(t_point ray, t_point hit_point, \
 	t = norm_square(chp) / dot_product(chp, cone->direction);
 	y = add_vectors(cone->coord, scalar_multi(t, cone->direction));
 	normal = unit_dist(hit_point, y);
+	if (texture)
+	{
+		color = get_uv_color(cone, texture, hit_point);
+		perturbation = (t_point){color.r - 0.5, color.g - 0.5, color.b - 0.5};
+		perturbation = scalar_multi(0.9, perturbation);
+		normal = unit_vector(add_vectors(perturbation, normal));
+	}
 	if (dot_product(unit_dist(hit_point, cone->center_base), \
 		cone->direction) >= -0.01)
 		normal = cone->direction;
--- src/print/print_plane.c
+++ src/print/print_plane.c
@@ -6,29 +6,60 @@
 /*   By: ncotte <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 14:36:36 by ncotte            #+#    #+#             */
-/*   Updated: 2023/02/22 01:05:50 by shalimi          ###   ########.fr       */
+/*   Updated: 2023/02/23 19:45:54 by shalimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+static t_color	plane_map(t_texture *tex, double x, double y)
+{
+	double	tu;
+	double	tv;
+
+	tu = y - floor(y);
+	tv = x - floor(x);
+	return (tex->pixels[(int)(tu * tex->height)][(int)(tv * tex->width)]);
+}
+
+static t_color	get_uv_color(t_plane *plane, t_texture *tex, \
+					t_point hit_point)
+{
+	double		tu;
+	double		tv;
+	double		x;
+	double		y;
+	t_point		vector;
+
+	vector = sub_vectors(hit_point, plane->coord);
+	x = (dot_product(vector, plane->right));
+	y = (dot_product(vector, plane->down));
+	tu = y - floor(y);
+	tv = x - floor(x);
+	return (tex->pixels[(int)(tu * tex->height)][(int)(tv * tex->width)]);
+}
+
+
 
 t_color	get_color_plane(t_scene *scene, t_object *object, \
 			t_point hit_point, t_point normal)
 {
 	t_plane	*plane;
 	t_point	vector;
-	long	x;
-	long	y;
+	double	x;
+	double	y;
 
 	(void) scene;
 	(void) normal;
 	plane = (t_plane *)object->object;
-	if (!object->color_bis)
+	if (!object->color_bis && !object->texture)
 		return (plane->color);
 	vector = sub_vectors(hit_point, plane->coord);
-	x = (long)floor(dot_product(vector, plane->right));
-	y = (long)floor(dot_product(vector, plane->down));
-	if (!(x % 2) ^ !(y % 2))
+	x = (dot_product(vector, plane->right));
+	y = (dot_product(vector, plane->down));
+	if (object->texture)
+		return plane_map(object->texture, x, y);
+	if (!((long)floor(x) % 2) ^ !((long)floor(y) % 2))
 		return (*object->color_bis);
 	return (plane->color);
 }
@@ -37,11 +68,22 @@ t_point	normal_plane(t_point ray, t_point hit_point, \
 			void *object, t_texture *texture)
 {
 	t_plane	*plane;
+	t_point		perturbation;
+	t_color		color;
+	t_point		normal;
 
-	(void) texture;
-	(void) hit_point;
 	plane = (t_plane *)object;
-	if (dot_product(plane->normal, ray) <= 0.0)
-		return (plane->normal);
-	return (scalar_multi(-1.0, plane->normal));
+	normal = plane->normal;
+	if (texture)
+	{
+		color = get_uv_color(plane, texture, hit_point);
+		perturbation = (t_point){color.r - 0.5, color.g - 0.5, color.b - 0.5};
+		perturbation = scalar_multi(0.9, perturbation);
+		normal = unit_vector(add_vectors(perturbation, normal));
+	}
+
+
+	if (dot_product(normal, ray) <= 0.0)
+		return (normal);
+	return (scalar_multi(-1.0, normal));
 }
